c5412fcea2732b99067df890173cc7bf8d681647
Gregory Shane <greg.shane00@gmail.com>
2023-12-08 17:34:18 -0800

    menu options added, select and place works, added font functionality and moved displayed stats

--- core/src/com/cs3a/tower/BasicTower.java
+++ core/src/com/cs3a/tower/BasicTower.java
@@ -3,7 +3,6 @@ package com.cs3a.tower;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
 
 public class BasicTower extends Tower {
     public BasicTower(){
